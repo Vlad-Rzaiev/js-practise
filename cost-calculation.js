819b50488d53d24a3f0512778026de075ebeb189
Vlad-Rzaiev <vlad.rzaev@gmail.com>
2024-12-29 16:32:28 +0000

    Deploying to gh-pages from @ Vlad-Rzaiev/js-practise@579cb2056821b9b8d7deca8c1cddc4037882377b 🚀

--- cost-calculation.js
+++ cost-calculation.js
@@ -1,7 +1,12 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const u=document.querySelector(".budget-form"),s=document.querySelector(".your-budget"),l=document.querySelector(".costs-form"),d=document.querySelector(".costs-table");let t;const r=e=>e%10===1&&e%100!==11?"гривня":[2,3,4].includes(e%10)&&![12,13,14].includes(e%100)?"гривні":"гривень",a=e=>{e.preventDefault(),t=Number(e.target.elements.budget.value),localStorage.setItem("userBudget",t),s.style.display="block",s.textContent=`Your budget is ${t} ${r(t)}.`,u.reset()},g=e=>{e.preventDefault();const n=e.target.elements.title.value,o=Number(e.target.elements.price.value);t=t-o,localStorage.setItem("userBudget",t),s.textContent=`Your budget is ${t} ${r(t)}.`;const c=`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";const i=document.querySelector(".budget-form"),r=document.querySelector(".your-budget"),m=document.querySelector(".costs-form"),a=document.querySelector(".costs-table"),u=document.querySelector(".total-price"),n=document.querySelector(".message");let s,l=0;const c=[];document.addEventListener("DOMContentLoaded",()=>{localStorage.getItem("userBudget")!==null&&(s=Number(localStorage.getItem("userBudget")),r.style.display="block",r.textContent=`Your budget is ${s} $.`);const t=JSON.parse(localStorage.getItem("user-products"))||[],o=t.map(e=>`
+        <tr class="costs-table-row">
+          <td class="costs-table-text">${e.name}</td>
+          <td class="costs-table-text">${e.price}</td>
+        </tr>
+    `).join("");a.insertAdjacentHTML("beforeend",o),u.textContent=`${t[t.length-1].total}`});const g=t=>{t.preventDefault(),s=Number(t.target.elements.budget.value),localStorage.setItem("userBudget",s),r.style.display="block",r.textContent=`Your budget is ${s} $`,t.target.reset()},b=t=>{t.preventDefault(),n.style.display="none";const o=t.target.elements.title.value.trim(),e=Number(t.target.elements.price.value);if(!o||isNaN(e)||e<=0){n.style.display="block",n.textContent="Please enter valid product name and price!";return}l+=e,u.textContent=`${l}`,c.push({name:o,price:e,total:l}),localStorage.setItem("user-products",JSON.stringify(c)),s-=e,localStorage.setItem("userBudget",s),r.textContent=`Your budget is ${s} $.`,n.style.display="block",n.textContent=`Product ${o} has added`;const d=`
     <tr class="costs-table-row">
-      <td class="costs-table-text">${n}</td>
       <td class="costs-table-text">${o}</td>
+      <td class="costs-table-text">${e}</td>
     </tr>
-  `;d.insertAdjacentHTML("beforeend",c),e.target.elements.title.focus(),t<=0&&(s.textContent="Over budget! You've spent all your money."),l.reset()};u.addEventListener("submit",a);l.addEventListener("submit",g);localStorage.getItem("userBudget")!==null&&(t=Number(localStorage.getItem("userBudget")),s.style.display="block",s.textContent=`Your budget is ${t} ${r(t)}.`);
+  `;a.insertAdjacentHTML("beforeend",d),t.target.elements.title.focus(),s<=0&&(r.textContent="Over budget! You've spent all your money."),t.target.reset()};i.addEventListener("submit",g);m.addEventListener("submit",b);
 //# sourceMappingURL=cost-calculation.js.map
