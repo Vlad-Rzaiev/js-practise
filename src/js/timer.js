7c52dca8b3ae79f3ca94b984930eb880492d9055
Vladyslav Rzaiev <vlad.rzaev@gmail.com>
2024-12-29 15:17:22 +0100

    6

--- src/js/timer.js
+++ src/js/timer.js
@@ -11,7 +11,9 @@ let hoursInput;
 let minutesInput;
 let secondsInput;
 let submitBtn;
-const alarm = new Audio('/src/sounds/clock-ticking.mp3');
+const alarm = new Audio(
+  'https://github.com/Vlad-Rzaiev/js-practise/blob/main/src/sounds/clock-ticking.mp3'
+);
 
 const calculateTotalSeconds = (days, hours, minutes, seconds) => {
   return days * 86400 + hours * 3600 + minutes * 60 + seconds;
